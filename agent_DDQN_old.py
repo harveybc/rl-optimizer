56a3913973fdcef803e0ab6185d33f491e9136af
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-07-24 12:25:23 -0500

    test
    test

--- agent_DDQN_old.py
+++ agent_DDQN_old.py
@@ -30,7 +30,7 @@ NUMVECTORS = 19
 VECTORSIZE = 48
 # skip for replay
 REPLAYFACTOR = 20
-BATCHSIZE = 10
+BATCHSIZE = 1
 MEMORYSIZE= 128000 #porque hay 1400 ticks y quiero recordar last 50
 REMEMBERTHRESHOLD = 1 #  frames to skip from remember if no action or change of balance is made
 STOPLOSS = 50000
