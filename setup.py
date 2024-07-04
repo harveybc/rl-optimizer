8996a66a5b48442bd5f84b2d5df60cb904f7880b
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-07-04 12:09:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,41 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='feature-extractor',
+    name='rl-optimizer',
     version='0.1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'feature_extractor=app.main:main'
+            'rl_optimizer=app.main:main'
         ],
-        'feature_extractor.encoders': [
-            'default=app.plugins.encoder_plugin_ann:Plugin',
-            'ann=app.plugins.encoder_plugin_ann:Plugin',
-            'rnn=app.plugins.encoder_plugin_rnn:Plugin',
-            'transformer=app.plugins.encoder_plugin_transformer:Plugin',
-            'lstm=app.plugins.encoder_plugin_lstm:Plugin',
-            'cnn=app.plugins.encoder_plugin_cnn:Plugin'
+        'rl_optimizer.optimizers': [
+            'neat=app.plugins.optimizer_plugin_neat:Plugin',
+            'neat_p2p=app.plugins.optimizer_plugin_neat_p2p:Plugin',
+            'openrl=app.plugins.optimizer_plugin_openrl:Plugin'
         ],
-        'feature_extractor.decoders': [
-            'default=app.plugins.decoder_plugin_ann:Plugin',
-            'ann=app.plugins.decoder_plugin_ann:Plugin',
-            'rnn=app.plugins.decoder_plugin_rnn:Plugin',
-            'transformer=app.plugins.decoder_plugin_transformer:Plugin',
-            'lstm=app.plugins.decoder_plugin_lstm:Plugin',
-            'cnn=app.plugins.decoder_plugin_cnn:Plugin'
+        'rl_optimizer.environments': [
+            'prediction_plugin=app.plugins.environment_plugin_prediction:Plugin'
+        ],
+        'rl_optimizer.agents': [
+            'agent_predictor=app.plugins.agent_plugin_predictor:Plugin'
         ]
     },
     install_requires=[
-        'keras',
         'numpy',
         'pandas',
         'scikit-learn',
-        'tensorflow'
+        'tensorflow',
+        'openrl',
+        'gym'
     ],
     author='Harvey Bastidas',
     author_email='your.email@example.com',
-    description='A feature extraction system that supports dynamic loading of encoder and decoder plugins for processing time series data.'
+    description='A reinforcement learning optimization system that supports dynamic loading of optimizer, environment, and agent plugins for processing and optimizing trading strategies.',
+    url='https://github.com/harveybc/rl-optimizer',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
