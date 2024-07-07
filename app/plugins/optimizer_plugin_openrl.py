d42ee5855860bf386f598fff0682c96c1bd5b10d
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-07-06 20:00:58 -0500

    Update optimizer_plugin_openrl.py

--- app/plugins/optimizer_plugin_openrl.py
+++ app/plugins/optimizer_plugin_openrl.py
@@ -6,35 +6,12 @@ import pickle
 
 class Config:
     def __init__(self, config_dict):
-        # Initialize required attributes with defaults
-        self.use_share_model = config_dict.get('use_share_model', False)
-        self.use_joint_action_loss = config_dict.get('use_joint_action_loss', False)
-        self.use_deepspeed = config_dict.get('use_deepspeed', False)
-        self.use_amp = config_dict.get('use_amp', False)
-        self.clip_param = config_dict.get('clip_param', 0.2)
-        self.huber_delta = config_dict.get('huber_delta', 1.0)
-        self.use_huber_loss = config_dict.get('use_huber_loss', True)
-        self.use_clipped_value_loss = config_dict.get('use_clipped_value_loss', True)
-        self.use_value_active_masks = config_dict.get('use_value_active_masks', False)
-        self.use_policy_active_masks = config_dict.get('use_policy_active_masks', False)
-        self.use_policy_vhead = config_dict.get('use_policy_vhead', False)
-        self.use_max_grad_norm = config_dict.get('use_max_grad_norm', True)
-        self.max_grad_norm = config_dict.get('max_grad_norm', 0.5)
-        self.ppo_epoch = config_dict.get('ppo_epoch', 4)
-        self.num_mini_batch = config_dict.get('num_mini_batch', 32)
-        self.data_chunk_length = config_dict.get('data_chunk_length', 10)
-        self.use_adv_normalize = config_dict.get('use_adv_normalize', True)
-        self.use_popart = config_dict.get('use_popart', False)
-        self.use_valuenorm = config_dict.get('use_valuenorm', False)
-        self.dual_clip_ppo = config_dict.get('dual_clip_ppo', False)
-        self.dual_clip_coeff = config_dict.get('dual_clip_coeff', 10.0)
-        self.entropy_coef = config_dict.get('entropy_coef', 0.01)
-        self.value_loss_coef = config_dict.get('value_loss_coef', 0.5)
-        self.policy_value_loss_coef = config_dict.get('policy_value_loss_coef', 0.5)
-        self.tpdv = config_dict.get('tpdv', None)
-        self.world_size = config_dict.get('world_size', 1)
-        # Add other config parameters
         self.__dict__.update(config_dict)
+        # Initialize additional attributes with default values if they are not present in config_dict
+        self.use_share_model = getattr(self, 'use_share_model', False)
+        self.use_joint_action_loss = getattr(self, 'use_joint_action_loss', False)
+        self.use_deepspeed = getattr(self, 'use_deepspeed', False)
+        self.world_size = getattr(self, 'world_size', 1)
 
 class Plugin:
     """
