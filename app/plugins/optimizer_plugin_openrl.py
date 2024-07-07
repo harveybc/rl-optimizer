f1c47578f07e01433fa5c324d531116a7de41ff5
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-07-07 00:45:24 -0500

    Update optimizer_plugin_openrl.py

--- app/plugins/optimizer_plugin_openrl.py
+++ app/plugins/optimizer_plugin_openrl.py
@@ -1,93 +1,9 @@
-import pandas as pd
-import numpy as np
-from openrl.algorithms.ppo import PPOAlgorithm
-from openrl.algorithms.dqn import DQNAlgorithm
-import pickle
 import torch
 import torch.optim as optim
 import torch.nn as nn
+import numpy as np
+import pickle
 
-
-class Config:
-    def __init__(self, config_dict):
-        self.__dict__.update(config_dict)
-        self.use_share_model = False
-        self.use_joint_action_loss = False
-        self.use_deepspeed = False
-        self.world_size = 1
-        self.clip_param = 0.2
-        self.ppo_epoch = 10
-        self.bc_epoch = 2
-        self.num_mini_batch = 1
-        self.mini_batch_size = 32
-        self.data_chunk_length = 10
-        self.policy_value_loss_coef = 0.5
-        self.value_loss_coef = 0.5
-        self.entropy_coef = 0.01
-        self.max_grad_norm = 0.5
-        self.huber_delta = 10.0
-        self.use_recurrent_policy = False
-        self.use_naive_recurrent_policy = False
-        self.use_max_grad_norm = True
-        self.use_clipped_value_loss = True
-        self.use_huber_loss = True
-        self.use_popart = False
-        self.use_valuenorm = True
-        self.use_value_active_masks = True
-        self.use_policy_active_masks = True
-        self.use_policy_vhead = False
-        self.use_adv_normalize = False
-        self.dec_actor = False
-        self.use_amp = False
-        self.dual_clip_ppo = False
-        self.dual_clip_coeff = 3.0
-
-class Plugin:
-    """
-    An optimizer plugin using PPO for reinforcement learning.
-    """
-
-    plugin_params = {
-        'lr': 0.0003,
-        'gamma': 0.99,
-        'lmbda': 0.95,
-        'eps_clip': 0.2,
-        'K_epochs': 4,
-        'hidden_dim': 64,
-        'genome_file': 'ppo_model.pkl'
-    }
-
-    plugin_debug_vars = ['lr', 'gamma', 'lmbda', 'eps_clip', 'K_epochs', 'hidden_dim']
-
-    def __init__(self):
-        self.params = self.plugin_params.copy()
-        self.environment = None
-        self.agent = None
-        self.optimizer = None
-
-    def set_params(self, **kwargs):
-        for key, value in kwargs.items():
-            self.params[key] = value
-
-    def get_debug_info(self):
-        return {var: self.params[var] for var in self.plugin_debug_vars}
-
-    def add_debug_info(self, debug_info):
-        plugin_debug_info = self.get_debug_info()
-        debug_info.update(plugin_debug_info)
-
-    def set_environment(self, environment):
-        self.environment = environment
-
-    def set_agent(self, agent):
-        self.agent = agent
-        self.optimizer = optim.Adam(self.agent.parameters(), lr=self.params['lr'])
-
-    def train(self, epochs):
-        gamma = self.params['gamma']
-        lmbda = self.params['lmbda']
-        eps_clip = self.params['eps_clip']
-        K_epochs = self.params['K_epochs']
 class Plugin:
     """
     An optimizer plugin using PPO for reinforcement learning.
@@ -146,8 +62,9 @@ class Plugin:
 
             for t in range(self.environment.max_steps):
                 policy_dist, value = self.agent(state)
+                policy_dist = torch.distributions.Normal(policy_dist, torch.ones_like(policy_dist))
                 action = policy_dist.sample()
-                log_prob = policy_dist.log_prob(action)
+                log_prob = policy_dist.log_prob(action).sum(dim=-1)
                 state, reward, done, _ = self.environment.step(action.numpy())
 
                 states.append(state)
@@ -179,7 +96,8 @@ class Plugin:
             # Update policy
             for _ in range(K_epochs):
                 policy_dist, value = self.agent(states)
-                log_probs = policy_dist.log_prob(actions)
+                policy_dist = torch.distributions.Normal(policy_dist, torch.ones_like(policy_dist))
+                log_probs = policy_dist.log_prob(actions).sum(dim=-1)
                 ratios = torch.exp(log_probs - old_log_probs)
 
                 surr1 = ratios * advantages
