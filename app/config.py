82d9880e1fe9ebfb51dc38595adacc1d84d89bce
Harvey Bastidas <harveybc@ingeni-us.com>
2024-08-17 15:34:46 -0500

    update

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@
 
 DEFAULT_VALUES = {
     'x_train_file': 'tests\\data\\x_d2_original.csv',
-    'y_train_file': '..\\Documents\\encoder_eval_d2_2.csv',
+    'y_train_file': '..\\Documents\\encoder_eval_d2_8.csv',
     'x_validation_file': 'tests\\data\\x_d3_original.csv',
-    'y_validation_file': '..\\Documents\\encoder_eval_d3_2.csv',
+    'y_validation_file': '..\\Documents\\encoder_eval_d3_8.csv',
     'target_column': None,
     'output_file': 'csv_output.csv',
     'save_model': 'model.keras',
@@ -24,7 +24,7 @@ DEFAULT_VALUES = {
     'quiet_mode': False,
     'force_date': False,
     'headers': True,
-    'max_steps': 20000,
+    'max_steps': 10000,
     'batch_size': 32,
     'epochs': 1000,
     'input_offset': 128,
