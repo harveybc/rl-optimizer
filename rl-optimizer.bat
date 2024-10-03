8c296acfbed1fccfb542d7629132a760e93fd2e6
Harvey Bastidas <harveybc@ingeni-us.com>
2024-10-03 10:49:01 -0500

    Update rl-optimizer.bat

--- rl-optimizer.bat
+++ rl-optimizer.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set PREV_PYTHONPATH = %PYTHONPATH%
-set PYTHONPATH=.\;..\gym-fx;..\test;%PREV_PYTHONPATH%
+set PYTHONPATH=.\;..\gym-fx;..\test;
 python app/main.py %*
 set PYTHONPATH=%PREV_PYTHONPATH%
 endlocal 
