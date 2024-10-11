3ce75c3d441c9a305b614944988473a9d90f65b2
Harvey Bastidas <harveybc@ingeni-us.com>
2024-10-10 19:07:26 -0500

    update

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@
 
 DEFAULT_VALUES = {
     'x_train_file': 'tests\\data\\x_d2.csv',
-    'y_train_file': '..\\Documents\\encoder_eval_d2_indicators_128.csv',
+    'y_train_file': 'tests\\data\\normalized_d2_target.csv',
     'x_validation_file': 'tests\\data\\x_d3.csv',
-    'y_validation_file': '..\\Documents\\encoder_eval_d3_indicators_128.csv',
+    'y_validation_file': 'tests\\data\\normalized_d3_target.csv',
     'target_column': None,
     'output_file': 'csv_output.csv',
     'save_model': 'model.keras',
