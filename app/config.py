c53e5d5d4fb9f2f430e15efc11aa9baa698185f0
Harvey Bastidas <harveybc@ingeni-us.com>
2024-10-26 00:45:33 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -26,7 +26,7 @@ DEFAULT_VALUES = {
     'quiet_mode': False,
     'force_date': False,
     'headers': True,
-    'max_steps': 6300,
+    'max_steps': 30000,
     'batch_size': 32,
     'epochs': 12,
     'input_offset': 127,
