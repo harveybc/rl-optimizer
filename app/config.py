7e5427be22a0fe0aa544e1a591deea28cfce0c6d
Harvey Bastidas <harveybc@ingeni-us.com>
2024-11-06 00:44:08 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ DEFAULT_VALUES = {
     'save_model': 'model.keras',
     'load_model': None,
     'evaluate_file': 'model_eval.csv',
-    'optimizer_plugin': 'neat_a_nomc_fb',
+    'optimizer_plugin': 'neat_a_nomc_v2',
     'environment_plugin': 'gym_fx_env_nomc_o_volume',
     'agent_plugin': 'neat_a_volume_v2',
     'remote_log': None,
