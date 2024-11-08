99e5a695647330cbc9d7598d81cbc8bc1dc5c3ce
Harvey Bastidas <harveybc@ingeni-us.com>
2024-11-08 08:45:39 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -28,7 +28,7 @@ DEFAULT_VALUES = {
     'headers': True,
     'max_steps': 6300,
     'batch_size': 32,
-    'epochs': 210,
+    'epochs': 105,
     'input_offset': 127,
     'mse_threshold': 0.001,
     'time_horizon': 0,
