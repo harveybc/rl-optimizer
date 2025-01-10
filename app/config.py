653b6c61985981b466bf99ac89649b95a601be97
Harvey Bastidas <harveybc@ingeni-us.com>
2025-01-10 15:01:29 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -35,7 +35,7 @@ DEFAULT_VALUES = {
     'periodicity_minutes': 60,
     'num_hidden': 0,
     'stabilization_cycles': 0,
-    'number_of_segments': 1,
+    'number_of_segments': 2,
     'overfitting_threshold': 0.5
 
 }
