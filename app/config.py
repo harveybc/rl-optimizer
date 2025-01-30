c556498f5ad9ba4a260a6f514c7234b57ca9fbc3
Harvey Bastidas <harveybc@ingeni-us.com>
2025-01-30 18:56:15 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -14,7 +14,7 @@ DEFAULT_VALUES = {
     'evaluate_file': 'model_eval.csv',
     'optimizer_plugin': 'neat_a_cs',
     'environment_plugin': 'gym_fx_env_nomc_o_volume',
-    'agent_plugin': 'neat_a_simple',
+    'agent_plugin': 'neat_a_volume',
     'remote_log': None,
     'remote_load_config': None,
     'remote_save_config': None,
